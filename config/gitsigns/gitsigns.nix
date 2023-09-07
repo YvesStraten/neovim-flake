e2b407d35314428c68dc7f39b574d7b987b48814
yvess <yvess.straten@gmail.com>
2023-09-07 09:22:17 +0800

    Made config more modular and added Neorg

---
+++ config/gitsigns/gitsigns.nix
@@ -0,0 +1,12 @@
+{
+  gitsigns = {
+    enable = true;
+    signs = {
+      add.text = "+";
+      change.text = "~";
+      changedelete.text = "-";
+      delete.text = "-";
+      delete.showCount = true;
+    };
+  };
+}
---
+++ config/neorg/neorg.nix
@@ -0,0 +1,26 @@
+{
+  neorg = {
+    enable = true;
+    modules = {
+      "core.defaults" = {
+        __empty = null;
+      };
+      "core.dirman" = {
+        config = {
+          workspaces = {
+            home = "~/org/";
+          };
+        };
+      };
+      "core.concealer" = {
+        __empty = null;
+      };
+      "core.export" = {
+        __empty = null;
+      };
+      "core.keybinds" = {
+        __empty = null;
+      };
+    };
+  };
+}
